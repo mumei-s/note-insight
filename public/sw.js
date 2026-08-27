43930b42971977980e2a74864009317c8b19c772
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-08-27 18:02:22 +0900

    Bust stale mobile cache after INSIGHT fixes

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v4";
+const CACHE_NAME = "mumei-note-insight-v5";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
