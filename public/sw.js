b2e50a04bf0e8ae1d85ffa57dbdef47a7574db1c
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-08-28 09:38:25 +0900

    Refresh cache for non-game INSIGHT updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v9";
+const CACHE_NAME = "mumei-note-insight-v10";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
