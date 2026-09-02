30ff1b162b792467be111de294b9035d2c6377c3
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-02 11:44:28 +0900

    release: bump cache for account-isolated access and notification sync

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v21";
+const CACHE_NAME = "mumei-note-insight-v22";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
