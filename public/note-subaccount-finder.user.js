a47296f35908372d4b5f92bbb5996b8c1baa39dd
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-02 19:32:14 +0900

    Release 巡回BOOST v4.9 automatic resume without button

--- public/note-subaccount-finder.user.js
+++ public/note-subaccount-finder.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         note 巡回BOOST｜タグ検索・スキ・マガジン v4.8.1
+// @name         note 巡回BOOST｜タグ検索・スキ・マガジン v4.9
 // @namespace    https://github.com/mumei-s/note-insight
-// @version      4.8.1
-// @description  v4.7の途中進捗・200件/日・自動再開・起動時新着読込を維持し、403個別スキップ、旧403停止ジョブ自動復旧、複数マガジン選択を追加。
+// @version      4.9.0
+// @description  v4.8.1の途中進捗・複数マガジン・403個別スキップ・200件/日・起動時新着読込を維持し、自動再開ON時は未完了ジョブをボタン操作なしで自動継続。
 // @match        https://note.com/*
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/77843a09e9bc671716af5fa0ac0448ef095eabdc/public/note-subaccount-finder.user.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/47874424d2f601f0c9c526d36c7c480860f9ce06/public/note-subaccount-finder-v44-patch.js
@@ -10,6 +10,7 @@
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/f2fd0aed0b5070ec356226240a71b4583f780e24/public/note-subaccount-finder-v46-magbatch.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/3bf5fb1ab11e60c2492d7433ead22cd7dc22812f/public/note-subaccount-finder-v47-autoresume.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/6ebedeef4659048a5431845aef13f3ba21e9c7eb/public/note-subaccount-finder-v48-multimag.js
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/576318e5788ef21df90afe820052136b694a21f8/public/note-subaccount-finder-v49-autostart.js
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
