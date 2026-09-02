94628042b5db795949784ff6ab0f9bf51ab03d4b
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-02 17:10:57 +0900

    release: bump cache for TOP launch fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v23";
+const CACHE_NAME = "mumei-note-insight-v24";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
