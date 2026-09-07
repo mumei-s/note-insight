0bee395cb327f537e33bc14f1ae82e045b9f1d76
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-07 19:49:00 +0900

    Bump INSIGHT app version independently to 2026.09.07.7

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -1,4 +1,4 @@
-export const CURRENT_INSIGHT_APP_VERSION = "2026.09.07.6";
+export const CURRENT_INSIGHT_APP_VERSION = "2026.09.07.7";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
 
 export type InsightRelease = {
