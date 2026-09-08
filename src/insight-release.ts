b43443eb920ae001ef11847573b52e2fdc86a60c
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-09 07:02:32 +0900

    Bump freeze hotfix release versions

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -1,5 +1,5 @@
-export const CURRENT_INSIGHT_APP_VERSION = "2026.09.09.1";
-export const CURRENT_NOTIFICATION_VERSION = "2.9.49";
+export const CURRENT_INSIGHT_APP_VERSION = "2026.09.09.2";
+export const CURRENT_NOTIFICATION_VERSION = "2.9.50";
 export const CURRENT_DASHBOARD_VERSION = "1.4.0";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
 export const DASHBOARD_VERSION_STORAGE_KEY = "mumei-dashboard-tool-version";
