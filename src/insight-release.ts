dec23d9795f46a1c0e980318858fef555ba5fc6d
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-08 19:31:11 +0900

    Bump INSIGHT app for full comment and avatar repair

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -1,4 +1,4 @@
-export const CURRENT_INSIGHT_APP_VERSION = "2026.09.08.6";
+export const CURRENT_INSIGHT_APP_VERSION = "2026.09.08.7";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
 
 export type InsightRelease = {
