6b434b1e189b82fba80eccee8222821bc1d47ebb
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-09 23:34:34 +0900

    Bump INSIGHT app to v2026.09.09.15

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -1,4 +1,4 @@
-export const CURRENT_INSIGHT_APP_VERSION = "2026.09.09.14";
+export const CURRENT_INSIGHT_APP_VERSION = "2026.09.09.15";
 export const CURRENT_NOTIFICATION_VERSION = "2.9.59";
 export const CURRENT_DASHBOARD_VERSION = "1.4.1";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
