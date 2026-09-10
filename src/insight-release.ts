4134b661616c7438ade8dc79dee2ca8778fe827a
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-11 04:38:26 +0900

    Load TOP installer dedupe guard

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -1,6 +1,7 @@
 import "./insight-mobile-v14";
 import "./insight-inline-updates-v1";
 import "./insight-mobile-v15";
+import "./insight-top-install-dedupe-v1";
 
 export const CURRENT_INSIGHT_APP_VERSION = "2026.09.11.3";
 export const CURRENT_NOTIFICATION_VERSION = "2.9.59";
