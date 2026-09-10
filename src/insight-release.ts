fe34bdc7185b7d6de3e579817f52a7c34e005789
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-10 21:33:01 +0900

    Load mobile v14 fixes and bump INSIGHT release

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -1,4 +1,6 @@
-export const CURRENT_INSIGHT_APP_VERSION = "2026.09.10.1";
+import "./insight-mobile-v14";
+
+export const CURRENT_INSIGHT_APP_VERSION = "2026.09.10.2";
 export const CURRENT_NOTIFICATION_VERSION = "2.9.59";
 export const CURRENT_DASHBOARD_VERSION = "1.4.2";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
