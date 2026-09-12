be011e77b49b892522c571ce8c89a86330be043a
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-12 14:27:10 +0900

    Publish INSIGHT app and notification v2.9.65 release

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -3,8 +3,8 @@ import "./insight-inline-updates-v1";
 import "./insight-top-install-v16";
 import "./insight-update-guide-v18";
 
-export const CURRENT_INSIGHT_APP_VERSION = "2026.09.11.5";
-export const CURRENT_NOTIFICATION_VERSION = "2.9.62";
+export const CURRENT_INSIGHT_APP_VERSION = "2026.09.12.1";
+export const CURRENT_NOTIFICATION_VERSION = "2.9.65";
 export const CURRENT_DASHBOARD_VERSION = "1.4.2";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
 export const DASHBOARD_VERSION_STORAGE_KEY = "mumei-dashboard-tool-version";
