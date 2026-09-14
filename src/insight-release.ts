57caee50eb2b041b96562b26e504dec6a5cbf809
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-14 18:22:34 +0900

    Bump INSIGHT and notification release for V3.1.5 recovery

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -1,10 +1,11 @@
 import "./insight-notification-enhancements-v17";
 import "./insight-inline-updates-v1";
 import "./insight-top-install-v16";
+import "./insight-notification-update-route-v1";
 import "./insight-update-guide-v18";
 
-export const CURRENT_INSIGHT_APP_VERSION = "2026.09.14.7";
-export const CURRENT_NOTIFICATION_VERSION = "3.1.4";
+export const CURRENT_INSIGHT_APP_VERSION = "2026.09.14.8";
+export const CURRENT_NOTIFICATION_VERSION = "3.1.5";
 export const CURRENT_DASHBOARD_VERSION = "1.4.4";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
 export const DASHBOARD_VERSION_STORAGE_KEY = "mumei-dashboard-tool-version";
