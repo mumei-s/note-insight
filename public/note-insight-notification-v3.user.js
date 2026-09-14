65e33aa777b3c587cbeddaf891915eb3e87b4ea6
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-14 18:42:13 +0900

    Release notification V3.1.6 with single display controller

--- public/note-insight-notification-v3.user.js
+++ public/note-insight-notification-v3.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         無名S note INSIGHT 本人通知 V3
 // @namespace    https://github.com/mumei-s/note-insight/notification-v3
-// @version      3.1.5
-// @description  本人通知V3。通知4ボタンの復旧、フィルター設定導線、更新後の即時INSIGHT復帰を強化した安定版。
+// @version      3.1.6
+// @description  本人通知V3。パネル表示制御を安定版1系統へ戻し、更新済み判定と一度だけのベル復旧を追加。
 // @match        https://note.com/*
 // @run-at       document-start
 // @grant        GM.xmlHttpRequest
@@ -13,7 +13,7 @@
 // @grant        GM_setValue
 // @connect      mumei-s.github.io
 // @connect      xxhaerjvrgmnadxjqetz.supabase.co
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-insight-notification-v315-fix.js?v=315
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-insight-notification-v316-fix.js?v=316
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/19b1beec55012b53bedcd93751af552f53c0cf8a/public/note-insight-notification-v3.user.js
 // @updateURL    https://mumei-s.github.io/note-insight/note-insight-notification-v3.user.js
 // @downloadURL  https://mumei-s.github.io/note-insight/note-insight-notification-v3.user.js
@@ -21,5 +21,5 @@
 
 (function(){
 'use strict';
-try{localStorage.setItem('mumei-notification-v3-wrapper','3.1.5')}catch{}
+try{localStorage.setItem('mumei-notification-v3-wrapper','3.1.6')}catch{}
 })();
