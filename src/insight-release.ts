ee6652c4dc9cc3e7595eca7e5161ac3aa6e5c1d5
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-14 10:11:30 +0900

    Align app release constants with notification V3.1.0

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -4,7 +4,7 @@ import "./insight-top-install-v16";
 import "./insight-update-guide-v18";
 
 export const CURRENT_INSIGHT_APP_VERSION = "2026.09.13.5";
-export const CURRENT_NOTIFICATION_VERSION = "3.0.1";
+export const CURRENT_NOTIFICATION_VERSION = "3.1.0";
 export const CURRENT_DASHBOARD_VERSION = "1.4.2";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
 export const DASHBOARD_VERSION_STORAGE_KEY = "mumei-dashboard-tool-version";
