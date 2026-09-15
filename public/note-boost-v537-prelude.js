e8dceac0bfde5e411f950c35f9b6451493c14b70
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-15 15:28:27 +0900

    Hide巡回BOOST UI on individual note article routes

--- public/note-boost-v537-prelude.js
+++ public/note-boost-v537-prelude.js
@@ -2,6 +2,25 @@
 'use strict';
 if(window.__NOTE_BOOST_V537_PRELUDE__)return;
 window.__NOTE_BOOST_V537_PRELUDE__=true;
+
+function isArticlePath(){
+  return /^\/[^/]+\/n\/[^/?#]+\/?$/.test(location.pathname);
+}
+function syncRoute(){
+  document.documentElement.classList.toggle('nb-boost-article-route',isArticlePath());
+}
+const style=document.createElement('style');
+style.textContent='.nb-boost-article-route #nb532-open,.nb-boost-article-route #nb532-panel,.nb-boost-article-route #nb532-modal{display:none!important;visibility:hidden!important;pointer-events:none!important}';
+(document.head||document.documentElement).appendChild(style);
+syncRoute();
+
+for(const name of ['pushState','replaceState']){
+  const orig=history[name];
+  history[name]=function(...args){const r=orig.apply(this,args);queueMicrotask(syncRoute);return r};
+}
+addEventListener('popstate',syncRoute);
+addEventListener('pageshow',syncRoute);
+
 const native=window.setInterval.bind(window);
 window.setInterval=function(fn,ms,...args){
   const src=typeof fn==='function'?Function.prototype.toString.call(fn):String(fn||'');
