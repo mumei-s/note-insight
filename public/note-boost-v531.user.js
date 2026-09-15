158a6bb5db804f95dcdfb32032b0f2d0f0121b89
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-16 00:13:19 +0900

    Release BOOST v5.4.4 magazine add verification fix

--- public/note-boost-v531.user.js
+++ public/note-boost-v531.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         note 巡回BOOST｜独立版 v5.4.3
+// @name         note 巡回BOOST｜独立版 v5.4.4
 // @namespace    https://github.com/mumei-s/note-insight
-// @version      5.4.3
-// @description  記事URL・マガジンURL・#から巡回。#巡回はnoteの専用ハッシュタグAPIを使用し完全一致。マガジン追加は現行APIのnumeric note_id payloadへ修正。
+// @version      5.4.4
+// @description  記事URL・マガジンURL・#から巡回。#巡回はnoteの専用ハッシュタグAPIを使用し完全一致。マガジン追加は実績のあるpayloadを復元し、反映確認まで実施。
 // @match        https://note.com/*
 // @updateURL    https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-boost-v531.user.js
 // @downloadURL  https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-boost-v531.user.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/5cebb388eafe41c208d3d68f66ee9b889ee754c7/public/note-boost-v542-prelude.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/aed33f76f20059e4f68ff06b2b9643e77831af0b/public/note-boost-v540-tag-owner.js
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/0bc9f4d3f8c283a2837df777f139923dd1222fb5/public/note-boost-v543-network-fix.js
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/a87ea5438461cc0ff0e382ee9cede1cf6a9bf439/public/note-boost-v543-network-fix.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/cc2afdc13d387301c2275907c87d2273331aba4a/public/note-boost-v531.user.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/48bf3abee32d40677a189da35d64d2b4b896650f/public/note-boost-v533-patch.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/fb232f7d612c23ef151c90e34f6e0b5619f54320/public/note-boost-v535-patch.js
@@ -18,5 +18,5 @@
 // ==/UserScript==
 (() => {
   'use strict';
-  // v5.4.3: hashtag専用API + magazine add payload修正。
+  // v5.4.4: restore verified v4.6 magazine payload + post-write verification.
 })();
