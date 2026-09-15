102fd484abede538d2b8ccf4b5966ecbe530a47b
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-15 19:23:17 +0900

    Release 巡回BOOST v5.4.2 with article-page heart panel

--- public/note-boost-v531.user.js
+++ public/note-boost-v531.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name         note 巡回BOOST｜独立版 v5.4.1
+// @name         note 巡回BOOST｜独立版 v5.4.2
 // @namespace    https://github.com/mumei-s/note-insight
-// @version      5.4.1
-// @description  記事URL・マガジンURL・#から巡回。#巡回はnoteの /hashtag/<タグ> 専用ページを直接取得し、実タグ完全一致の記事だけを採用。一般キーワード検索APIは使用しない。
+// @version      5.4.2
+// @description  記事URL・マガジンURL・#から巡回。#巡回はnoteの /hashtag/<タグ> 専用ページを直接取得し、実タグ完全一致の記事だけを採用。巡回先の個別記事でも💗パネルを常時操作可能。
 // @match        https://note.com/*
-// @exclude      https://note.com/*/n/*
 // @updateURL    https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-boost-v531.user.js
 // @downloadURL  https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-boost-v531.user.js
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/526f236fd162ee73ce921d0a7294883a30de8c1a/public/note-boost-v537-prelude.js
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/5cebb388eafe41c208d3d68f66ee9b889ee754c7/public/note-boost-v542-prelude.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/8989a27908cdcb7882f602a1d896363612e6c9be/public/note-boost-v540-tag-owner.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/cc2afdc13d387301c2275907c87d2273331aba4a/public/note-boost-v531.user.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/48bf3abee32d40677a189da35d64d2b4b896650f/public/note-boost-v533-patch.js
@@ -18,5 +17,5 @@
 // ==/UserScript==
 (() => {
   'use strict';
-  // v5.4.1: #巡回の入口を一般検索からnote公式ハッシュタグページへ切替。
+  // v5.4.2: #巡回は公式ハッシュタグページ。個別記事でも💗タブ/パネルを表示。
 })();
