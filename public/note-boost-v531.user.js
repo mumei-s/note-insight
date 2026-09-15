88271422fa4b3ce68f7a03c8b73cd295d4f6ebbe
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-15 15:00:17 +0900

    Release巡回BOOST v5.3.7 flicker-free UI

--- public/note-boost-v531.user.js
+++ public/note-boost-v531.user.js
@@ -1,19 +1,20 @@
 // ==UserScript==
-// @name         note 巡回BOOST｜独立版 v5.3.6
+// @name         note 巡回BOOST｜独立版 v5.3.7
 // @namespace    https://github.com/mumei-s/note-insight
-// @version      5.3.6
-// @description  記事URL・マガジンURL・#から巡回。現在の人が元からスキ済み/未スキ/今回スキ済みかを専用帯で即判定し、記事表示時は巡回UIを消して同一タブ遷移、マガジン実サムネイルも補完。
+// @version      5.3.7
+// @description  記事URL・マガジンURL・#から巡回。周期再描画を廃止し、現在人物のスキ状態・リミッター・マガジンサムネをイベント時だけ更新。♡極小ボタンはパネル表示中も常時維持。
 // @match        https://note.com/*
 // @updateURL    https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-boost-v531.user.js
 // @downloadURL  https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-boost-v531.user.js
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/ba4361c8f32d4ca559a62c3fbb0adea74261d4d8/public/note-boost-v537-prelude.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/cc2afdc13d387301c2275907c87d2273331aba4a/public/note-boost-v531.user.js
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/f32951bfedc922a16d22574231d340371ef85956/public/note-boost-v533-patch.js
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/167589306e9267ce9280c0e8ea50c27d6b922561/public/note-boost-v535-patch.js
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/811efee890447a02c516fe75e2902a12f980b579/public/note-boost-v536-clarity.js
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/48bf3abee32d40677a189da35d64d2b4b896650f/public/note-boost-v533-patch.js
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/fb232f7d612c23ef151c90e34f6e0b5619f54320/public/note-boost-v535-patch.js
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/2c4f54bf6756302fefe4faceb1a491143df5a32a/public/note-boost-v536-clarity.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 (() => {
   'use strict';
-  // v5.3.2巡回コア + v5.3.4リミッター/UI + v5.3.5記事遷移/サムネ補完 + v5.3.6現在人物の明確なスキ状態表示。
+  // v5.3.7: 旧周期再描画を止め、表示更新をイベント駆動へ統一。
 })();
