3f900fa9e5cf89c0bb22b9b96c7485892be96a45
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-15 21:27:35 +0900

    Release BOOST v5.4.3 hashtag API and magazine add fix

--- public/note-boost-v531.user.js
+++ public/note-boost-v531.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name         note 巡回BOOST｜独立版 v5.4.2
+// @name         note 巡回BOOST｜独立版 v5.4.3
 // @namespace    https://github.com/mumei-s/note-insight
-// @version      5.4.2
-// @description  記事URL・マガジンURL・#から巡回。#巡回はnoteの /hashtag/<タグ> 専用ページを直接取得し、実タグ完全一致の記事だけを採用。巡回先の個別記事でも💗パネルを常時操作可能。
+// @version      5.4.3
+// @description  記事URL・マガジンURL・#から巡回。#巡回はnoteの専用ハッシュタグAPIを使用し完全一致。マガジン追加は現行APIのnumeric note_id payloadへ修正。
 // @match        https://note.com/*
 // @updateURL    https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-boost-v531.user.js
 // @downloadURL  https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-boost-v531.user.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/5cebb388eafe41c208d3d68f66ee9b889ee754c7/public/note-boost-v542-prelude.js
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/8989a27908cdcb7882f602a1d896363612e6c9be/public/note-boost-v540-tag-owner.js
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/aed33f76f20059e4f68ff06b2b9643e77831af0b/public/note-boost-v540-tag-owner.js
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/0bc9f4d3f8c283a2837df777f139923dd1222fb5/public/note-boost-v543-network-fix.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/cc2afdc13d387301c2275907c87d2273331aba4a/public/note-boost-v531.user.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/48bf3abee32d40677a189da35d64d2b4b896650f/public/note-boost-v533-patch.js
 // @require      https://raw.githubusercontent.com/mumei-s/note-insight/fb232f7d612c23ef151c90e34f6e0b5619f54320/public/note-boost-v535-patch.js
@@ -17,5 +18,5 @@
 // ==/UserScript==
 (() => {
   'use strict';
-  // v5.4.2: #巡回は公式ハッシュタグページ。個別記事でも💗タブ/パネルを表示。
+  // v5.4.3: hashtag専用API + magazine add payload修正。
 })();
