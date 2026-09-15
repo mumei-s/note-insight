93fda3f4d5ebe7584d1c4be0fbb48fd44a1e19ae
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-15 21:15:52 +0900

    Track notification V3.2.5 release

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -6,8 +6,8 @@ import "./insight-top-install-v16";
 import "./insight-notification-update-route-v1";
 import "./insight-update-guide-v18";
 
-export const CURRENT_INSIGHT_APP_VERSION = "2026.09.15.5";
-export const CURRENT_NOTIFICATION_VERSION = "3.2.4";
+export const CURRENT_INSIGHT_APP_VERSION = "2026.09.15.6";
+export const CURRENT_NOTIFICATION_VERSION = "3.2.5";
 export const CURRENT_DASHBOARD_VERSION = "1.4.4";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
 export const DASHBOARD_VERSION_STORAGE_KEY = "mumei-dashboard-tool-version";
