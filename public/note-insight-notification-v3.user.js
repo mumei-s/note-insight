28346de5cae462a7544533b42447efe06b4acb52
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-15 11:48:01 +0900

    Release本人通知 V3.2.1

--- public/note-insight-notification-v3.user.js
+++ public/note-insight-notification-v3.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         無名S note INSIGHT 本人通知 V3
 // @namespace    https://github.com/mumei-s/note-insight/notification-v3
-// @version      3.2.0
+// @version      3.2.1
 // @description  本人通知V3。通知一覧だけに1段4列パネルを表示し、読込・フィルター・INSIGHT連携・ダッシュボード同期を1本に統合。
 // @match        https://note.com/*
 // @match        https://mumei-s.github.io/note-insight/tool-setup.html*
@@ -17,14 +17,14 @@
 // @grant        GM_setValue
 // @connect      mumei-s.github.io
 // @connect      xxhaerjvrgmnadxjqetz.supabase.co
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-insight-notification-fixed-dock-v319.js?v=320
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-insight-notification-dock-watch-v312.js?v=320
-// @require      https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-insight-notification-loader-v318.js?v=320
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-insight-notification-fixed-dock-v319.js?v=321
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-insight-notification-dock-watch-v312.js?v=321
+// @require      https://raw.githubusercontent.com/mumei-s/note-insight/main/public/note-insight-notification-loader-v318.js?v=321
 // @updateURL    https://mumei-s.github.io/note-insight/note-insight-notification-v3.user.js
 // @downloadURL  https://mumei-s.github.io/note-insight/note-insight-notification-v3.user.js
 // ==/UserScript==
 
 (function(){
 'use strict';
-try{localStorage.setItem('mumei-notification-v3-wrapper','3.2.0')}catch{}
+try{localStorage.setItem('mumei-notification-v3-wrapper','3.2.1')}catch{}
 })();
