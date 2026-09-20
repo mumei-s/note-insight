d2d59ec1154c4ea3299084c7d4af3cc865fbbf81
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-21 07:34:27 +0900

    Refresh INSIGHT service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v31";
+const CACHE_NAME = "mumei-note-insight-v32";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg", "./recovery.html"];
 
 self.addEventListener("install", (event) => {
