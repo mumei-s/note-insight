20d6eff4388618775f0409e066f1f32c3bdfcb30
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-21 09:33:08 +0900

    Bump service worker cache for 2026.09.21.17

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v34";
+const CACHE_NAME = "mumei-note-insight-v35";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg", "./recovery.html"];
 
 self.addEventListener("install", (event) => {
