9fce4479009dc666e672147c344fd950ffa29015
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-21 21:25:53 +0900

    Rotate PWA cache for INSIGHT 2026.09.21.28

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v43";
+const CACHE_NAME = "mumei-note-insight-v44";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg", "./recovery.html"];
 
 self.addEventListener("install", (event) => {
