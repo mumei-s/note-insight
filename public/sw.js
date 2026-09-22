6e291e12e562798e00e8e6732535eb3c88f6422c
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-22 15:08:51 +0900

    release: refresh cache for DM composer fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v48";
+const CACHE_NAME = "mumei-note-insight-v49";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg", "./recovery.html"];
 
 self.addEventListener("install", (event) => {
