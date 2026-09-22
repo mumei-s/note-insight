3270130e8c2c41235414f50ee4afbe659a76da29
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-22 20:25:51 +0900

    release: refresh the application shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v52";
+const CACHE_NAME = "mumei-note-insight-v53";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg", "./recovery.html"];
 
 self.addEventListener("install", (event) => {
