427380ad35cd0444dabd6a515970dac857e50a4e
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-22 11:10:32 +0900

    Rotate app cache for the September 22 continuation

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v44";
+const CACHE_NAME = "mumei-note-insight-v45";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg", "./recovery.html"];
 
 self.addEventListener("install", (event) => {
