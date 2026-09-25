8556a96ee12073f2ef9cbc1433a3b7da0d6ec3ce
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-25 16:10:59 +0900

    Publish note editor safety versions in insight-release.ts

--- src/insight-release.ts
+++ src/insight-release.ts
@@ -7,9 +7,9 @@ import "./insight-notification-update-route-v1";
 import "./insight-update-guide-v18";
 
 export const CURRENT_INSIGHT_APP_VERSION = "2026.09.22.12";
-export const CURRENT_NOTIFICATION_VERSION = "3.6.6";
+export const CURRENT_NOTIFICATION_VERSION = "3.6.7";
 export const CURRENT_DASHBOARD_VERSION = "1.4.9";
-export const CURRENT_DM_VERSION = "1.4.5";
+export const CURRENT_DM_VERSION = "1.4.6";
 export const NOTIFICATION_VERSION_STORAGE_KEY = "mumei-notification-tool-version";
 export const DASHBOARD_VERSION_STORAGE_KEY = "mumei-dashboard-tool-version";
 export const DM_VERSION_STORAGE_KEY = "mumei-dm-tool-version";
