603e83c36dd33600d4cda99edef44e99de64f441
無名S note <ss526s-shigoto@yahoo.co.jp>
2026-09-25 16:19:42 +0900

    Rotate PWA cache for editor safety release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mumei-note-insight-v56";
+const CACHE_NAME = "mumei-note-insight-v57";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg", "./recovery.html"];
 
 self.addEventListener("install", (event) => {
